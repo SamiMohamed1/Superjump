5a81eaf6ceea0e65aebd22e373271274c6a1924d
Suwn110 <samimohamed0811@gmail.com>
2023-03-18 19:50:11 +0100

    hele game

---
+++ src/main/java/org/entitie/speler/Speler.java
@@ -0,0 +1,74 @@
+package org.entitie.speler;
+
+import com.github.hanyaeger.api.AnchorPoint;
+import com.github.hanyaeger.api.Coordinate2D;
+import com.github.hanyaeger.api.Size;
+import com.github.hanyaeger.api.entities.*;
+import com.github.hanyaeger.api.entities.impl.DynamicSpriteEntity;
+import com.github.hanyaeger.api.scenes.SceneBorder;
+import com.github.hanyaeger.api.userinput.KeyListener;
+import javafx.scene.input.KeyCode;
+import org.map.Grond;
+
+import java.util.Set;
+
+public class Speler extends DynamicSpriteEntity implements KeyListener, Newtonian, SceneBorderCrossingWatcher, SceneBorderTouchingWatcher, Collided {
+    private int health = 10;
+
+    public Speler(Coordinate2D Location) {
+        super("afbeeldingen/testarcher1.png", Location, new Size(50, 50));
+        setFrictionConstant(0.05);
+    }
+
+    @Override
+    public void onPressedKeysChange(Set<KeyCode> pressedKeys) {
+        if (pressedKeys.contains(KeyCode.LEFT)) {
+            addToMotion(13, 270d);
+        } else if (pressedKeys.contains(KeyCode.RIGHT)) {
+            addToMotion(13, 90d);
+        } else if (pressedKeys.contains(KeyCode.UP)) {
+            addToMotion(13, 180d);
+        } else if (pressedKeys.contains(KeyCode.DOWN)) {
+            addToMotion(13, 0d);
+        }
+    }
+
+
+    @Override
+    public void notifyBoundaryCrossing(SceneBorder border) {
+        switch (border) {
+            case BOTTOM:
+                setAnchorLocationY(getSceneHeight() - getHeight() - 1);
+                break;
+            case LEFT:
+                setAnchorLocationX(getSceneWidth() - getWidth() - 10);
+                break;
+            case RIGHT:
+                setAnchorLocationX(10);
+            default:
+                break;
+        }
+    }
+
+    @Override
+    public void notifyBoundaryTouching(SceneBorder sceneBorder) {
+        switch (sceneBorder) {
+            case TOP:
+                setAnchorLocationY(1);
+                break;
+        }
+    }
+
+    @Override
+    public void onCollision(Collider collidingObject) {
+        if (collidingObject instanceof Grond) {
+            System.out.println(getAnchorLocation());
+            System.out.println(getAnchorPoint());
+            System.out.println("---------------------------");
+//            nullifySpeedInDirection(Direction.DOWN);
+            setAnchorLocationY(513);
+        }
+    }
+
+
+}
---
+++ src/main/java/org/knoppen/Knoppen.java
@@ -0,0 +1,33 @@
+package org.knoppen;
+
+import com.github.hanyaeger.api.AnchorPoint;
+import com.github.hanyaeger.api.Coordinate2D;
+import com.github.hanyaeger.api.entities.impl.TextEntity;
+import com.github.hanyaeger.api.userinput.MouseButtonPressedListener;
+import com.github.hanyaeger.api.userinput.MouseEnterListener;
+import javafx.scene.Cursor;
+import javafx.scene.input.MouseButton;
+import javafx.scene.paint.Color;
+import org.SuperJumpGame;
+
+public class Knoppen extends TextEntity implements MouseButtonPressedListener, MouseEnterListener {
+    private SuperJumpGame superJumpGame;
+    public Knoppen(Coordinate2D initialLocation, SuperJumpGame superJumpGame) {
+        super(initialLocation,"Starten");
+        this.superJumpGame = superJumpGame;
+        setAnchorPoint(AnchorPoint.CENTER_CENTER);
+        setFill(Color.BLACK);
+    }
+
+    @Override
+    public void onMouseButtonPressed(MouseButton mouseButton, Coordinate2D coordinate2D) {
+        superJumpGame.setActiveScene(1);
+        System.out.println(22222);
+    }
+
+    @Override
+    public void onMouseEntered() {
+        setFill(Color.VIOLET);
+        setCursor(Cursor.HAND);
+    }
+}
