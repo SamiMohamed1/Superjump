0728d58c64adbf8b15a39e35b2fc6aa27451b159
Suwn110 <samimohamed0811@gmail.com>
2023-03-24 22:26:51 +0100

    map +items

--- src/main/java/org/entitie/vijand/Vijand.java
+++ src/main/java/org/entitie/vijand/Vijand.java
@@ -7,6 +7,7 @@ import com.github.hanyaeger.api.scenes.SceneBorder;
 import com.github.hanyaeger.api.userinput.KeyListener;
 import javafx.scene.input.KeyCode;
 import org.PlayerCollision;
+import org.entitie.speler.Speler;
 
 import java.util.Set;
 
