c29917e5895c0474759008e34c85a66b8db99bd1
Suwn110 <samimohamed0811@gmail.com>
2023-03-24 13:01:36 +0100

    projectielen

---
+++ src/main/java/org/entitie/Projectiel.java
@@ -0,0 +1,12 @@
+package org.entitie;
+
+import com.github.hanyaeger.api.Coordinate2D;
+import com.github.hanyaeger.api.entities.impl.DynamicSpriteEntity;
+import com.github.hanyaeger.api.entities.impl.SpriteEntity;
+
+public class Projectiel extends DynamicSpriteEntity {
+    public Projectiel(String resource, Coordinate2D initialLocation) {
+        super(resource, initialLocation);
+//        setMotion();
+    }
+}
