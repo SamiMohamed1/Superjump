5794b1c57de1cc91e858e3d4605eacc5fc716cef
Suwn110 <samimohamed0811@gmail.com>
2023-03-27 11:21:18 +0200

    vijand krijgt damage van projectiel

--- src/main/java/org/entitie/projectiel/ProjectielCollision.java
+++ src/main/java/org/entitie/projectiel/ProjectielCollision.java
@@ -1,6 +1,7 @@
 package org.entitie.projectiel;
 
 public interface ProjectielCollision {
+
     public void spelerProjectilCollision(int spelersterkte);
     public void enemyProjectilCollision();
 }
